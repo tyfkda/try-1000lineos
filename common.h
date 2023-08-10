342827f9476179c573803fde2ac08d6bd82834cf
tyfkda <tyfkda@gmail.com>
2023-08-10 14:41:59 +0900

    ユーティリティ関数
    C標準にあるようなやつ。

--- common.h
+++ common.h
@@ -1,8 +1,30 @@
 #pragma once
 
+typedef int bool;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+typedef uint32_t size_t;
+typedef uint32_t paddr_t;
+typedef uint32_t vaddr_t;
+
+#define true  1
+#define false 0
+#ifndef NULL
+#define NULL  ((void *) 0)
+#endif
+#define align_up(value, align)   __builtin_align_up(value, align)
+#define is_aligned(value, align) __builtin_is_aligned(value, align)
+#ifndef offsetof
+#define offsetof(type, member)   __builtin_offsetof(type, member)
+#endif
 #define va_list  __builtin_va_list
 #define va_start __builtin_va_start
 #define va_end   __builtin_va_end
 #define va_arg   __builtin_va_arg
 
+void *memset(void *buf, char c, size_t n);
+void *memcpy(void *dst, const void *src, size_t n);
+char *strcpy(char *dst, const char *src);
 void printf(const char *fmt, ...);
