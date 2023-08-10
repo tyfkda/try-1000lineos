3e79c2468ce2a5f193a7179694413dffc3128890
tyfkda <tyfkda@gmail.com>
2023-08-10 10:40:26 +0900

    コンパイルして実行できるようにする
    無限ループで停止させるところまで。
    
    C-A c で中断、
    `info registers` でレジスタ情報を見る。
    `q` で終了。

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -xue
+
+# QEMUのファイルパス
+QEMU=qemu-system-riscv32
+# CC=/opt/homebrew/opt/llvm/bin/clang
+CC=clang
+
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# カーネルをビルド
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
+
+# QEMUを起動
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
